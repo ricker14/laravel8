0f0080f69c8de06fd7ac5479a99adc7435392737
Rick O'Connor <rick.oconnor@cashtn.com>
2021-09-10 10:10:27 -0500

    Added new route Added new view for the route Added a template that loads the css and js Fixed a PSR issue

---
+++ resources/views/shared/app.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=no" />
+
+    <title>Cash Express Audit Portal - @yield('title')</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="../public/semantic.css">
+
+    <!-- Scripts -->
+    <script
+        src="https://code.jquery.com/jquery-3.1.1.min.js"
+        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
+        crossorigin="anonymous">
+    </script>
+    <script src="../public/semantic.js"></script>
+
+</head>
+
+<body>
+
+@yield('content')
+
+</body>
---
+++ resources/views/start.blade.php
@@ -0,0 +1,10 @@
+@extends('shared.layouts.app')
+
+@section('content')
+    <div class="ui segment">
+        <h3 class="ui header">First Page</h3>
+        <div class="field">
+            Some stuff
+        </div>
+    </div>
+@endsection
