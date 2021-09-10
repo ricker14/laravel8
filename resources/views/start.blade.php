3b9886dec053546569de10f55e9b93b411e7e054
Rick O'Connor <rick.oconnor@cashtn.com>
2021-09-10 13:59:22 -0500

    Add grid styling

--- resources/views/start.blade.php
+++ resources/views/start.blade.php
@@ -3,8 +3,11 @@
 @section('content')
     <div class="ui segment">
         <h3 class="ui header">First Page</h3>
-        <div class="field">
-            Some stuff
+        <div class="ui grid">
+            <div class="four wide column">One</div>
+            <div class="four wide column">Two</div>
+            <div class="four wide column">Three</div>
+            <div class="four wide column">Four</div>
         </div>
     </div>
 @endsection
